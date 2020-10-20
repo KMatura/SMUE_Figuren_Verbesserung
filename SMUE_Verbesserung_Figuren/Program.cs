1c4dc942968b130a4ade14bc6d090966b542cb5c
KMatura <matura.k04@htlwienwest.at>
2020-10-20 16:37:53 +0200

    SMUE_Verbesserung

--- SMUE_Verbesserung_Figuren/Program.cs
+++ SMUE_Verbesserung_Figuren/Program.cs
@@ -6,10 +6,44 @@ using System.Threading.Tasks;
 
 namespace SMUE_Verbesserung_Figuren
 {
+    abstract class Figur
+    {
+        public abstract int GetAnzahlDerEcken();
+    }
+    class Dreieck : Figur
+    {
+        public override int GetAnzahlDerEcken()
+        {
+            return 3;
+        }
+    }
+    class Rechteck : Figur
+    {
+        public override int GetAnzahlDerEcken()
+        {
+            return 4;
+        }
+    }
     class Program
     {
+
+        public static int summeEcken(List<Figur> figuren)
+        {
+            int anzahl = 0;
+            foreach (Figur item in figuren)
+            {
+                anzahl += item.GetAnzahlDerEcken();
+            }
+            return anzahl;
+        }
         static void Main(string[] args)
         {
+            List<Figur> figuren = new List<Figur>();
+            figuren.Add(new Dreieck());
+            figuren.Add(new Rechteck());
+            Console.WriteLine($"Summe der Ecken: {summeEcken(figuren)}");
+            Console.ReadKey();
+            //
         }
     }
 }
